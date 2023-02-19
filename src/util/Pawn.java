61c257592e92d236b9ac39e9a965a599688e10e5
Julian Guerra <33579118+scatmanspace@users.noreply.github.com>
2023-02-19 17:32:55 -0500

    Initial commit

---
+++ src/util/Pawn.java
@@ -0,0 +1,38 @@
+package util;
+
+import game.*;
+
+public class Pawn extends Piece {
+    public Pawn(boolean white) {
+        super(white);
+    }
+
+    @Override
+    public boolean isLegal(Move move, Game game) {
+        if (!super.isLegal(move, game))
+            return false;
+        //rules for pawn only!
+        int rowDiff = move.getRow1() - move.getRow0();
+        int colDiff = move.getCol1() - move.getCol0();
+        if (rowDiff > 0 && white || rowDiff < 0 && !white)
+            return false;//pawn doesn't move backward!
+        //add more rules here
+        if (game.getPiece(move.getRow1(), move.getCol1()) == null) { // moving not capturing
+            if (colDiff != 0 || Math.abs(rowDiff) > 2)    // if moving sideways or more than two spaces return false
+                return false;
+            Boolean initialSpace = move.getRow0() != 2 && !white || move.getRow0() != 6 && white;
+            if (Math.abs(rowDiff) > 1 && initialSpace) // if moving more than one space
+                return false;
+        } else { // capturing
+            if (Math.abs(colDiff) != 1 && Math.abs(rowDiff) != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return white ? "\u2659" : "\u265F";
+    }
+}
